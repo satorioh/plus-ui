7e0c61a03904de5a594990da3bdc3d2d59f01e80
Robin.Wang <3Jg$94sbQ@q1>
2025-02-27 10:21:36 +0800

    feat(project): change backend ip

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
       open: true,
       proxy: {
         [env.VITE_APP_BASE_API]: {
-          target: 'http://localhost:8080',
+          target: 'http://192.168.19.11:8080',
           changeOrigin: true,
           ws: true,
           rewrite: (path) => path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '')
